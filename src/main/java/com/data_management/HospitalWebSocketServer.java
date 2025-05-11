dd81910f17ac8be29c20702b7c84b2ace0f1340b
IvaPapukchieva <i.papukchieva@student.maastrichtuniversity.nl>
2025-05-11 12:35:36 +0200

    week five minor progress

---
+++ src/main/java/com/data_management/HospitalWebSocketServer.java
@@ -0,0 +1,57 @@
+package com.data_management;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import java.net.InetSocketAddress;
+
+public class HospitalWebSocketServer extends WebSocketServer {
+
+
+    public HospitalWebSocketServer(int port) {
+        super(new InetSocketAddress(port));
+
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        System.out.println("New client connected: " + conn.getRemoteSocketAddress());
+        conn.send("Welcome client!");
+
+    }
+
+    // Called when a client disconnects
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        System.out.println("Client disconnected: " + conn.getRemoteSocketAddress());
+    }
+
+    // Called when a message is received
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        System.out.println("Received: " + message);
+        conn.send("You said: " + message); // Echo the message
+    }
+
+    // Called when an error occurs
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        ex.printStackTrace();
+
+    }
+
+    // Called when the server starts
+    @Override
+    public void onStart() {
+        System.out.println("Server started");
+    }
+
+    public static void main(String[] args) {
+        HospitalWebSocketServer server = new HospitalWebSocketServer(8887); // Port 8887
+        server.start(); // Start the server
+    }
+
+}
+
+
